2bccb00fecf713530df1b5b012e171e3bc78d784
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-06-11 23:32:26 -0300

    Agregar componente ContainerScrollSection: implementación de una sección con desplazamiento y texto estilizado.

---
+++ src/components/interfaces/Feature2.tsx
@@ -0,0 +1,45 @@
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+import { AuroraText } from "@/components/magicui/aurora-text";
+
+export default function ContainerScrollSection() {
+    return (
+        <section className="">
+            <div className="container mx-auto">
+                <ContainerScroll
+                    titleComponent={
+                        <>
+                            <p className="mx-auto mb-4 text-muted-foreground md:text-xl">
+                                Obtenga una interfaz de usuario hermosa y
+                                moderna
+                            </p>
+                            <h1 className="text-7xl font-semibold tracking-tighter md:text-8xl xl:text-[10rem]">
+                                <span className="pr-6 text-muted-foreground/40">
+                                    Con
+                                </span>
+                                <AuroraText
+                                    colors={[
+                                        "#ff6449",
+                                        "#ff6449",
+                                        "#6248fe",
+                                        "#6248fe",
+                                    ]}
+                                >
+                                    Devs Hub
+                                </AuroraText>
+                            </h1>
+                        </>
+                    }
+                >
+                    <img
+                        src="https://deifkwefumgah.cloudfront.net/shadcnblocks/block/dashboard/admin-dashboard-1.png"
+                        alt="hero"
+                        height={720}
+                        width={1400}
+                        className="mx-auto h-full rounded-2xl object-cover object-left-top"
+                        draggable={false}
+                    />
+                </ContainerScroll>
+            </div>
+        </section>
+    );
+}
