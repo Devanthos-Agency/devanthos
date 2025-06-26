a99fac839e178990448e8d16de6c3c9176314bfe
Juan Domingo Exequiel Morales <85465719+Juanmorales1810@users.noreply.github.com>
2025-06-25 23:06:20 -0300

    Actualizar la colección de blog para incluir campos de resumen, etiqueta, autor y fecha de publicación

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,11 +7,13 @@ const blog = defineCollection({
     // Type-check frontmatter using a schema
     schema: z.object({
         title: z.string(),
-        description: z.string(),
-        // Transform string to Date object
-        pubDate: z.coerce.date(),
+        summary: z.string(),
+        label: z.string(),
+        author: z.string(),
+        published: z.coerce.date(),
         updatedDate: z.coerce.date().optional(),
         heroImage: z.string().optional(),
+        imageAvatar: z.string().optional(),
     }),
 });
 
