2bc272a1980861a42deac7d7ac90e7ec76b84d1a
Juanmorales1810 <juan1810.jm@gmail.com>
2025-07-16 09:41:39 -0300

    fix: unificar el estilo de comillas en astro.config.mjs y agregar archivo robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    site: "https://www.devanthos.com",
 
-  integrations: [react(), mdx(), sitemap()]
+    integrations: [react(), mdx(), sitemap()],
 });
