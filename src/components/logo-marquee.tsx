92617203a73a52cd6aa3b905c93c51754357e765
Juanmorales1810 <juan1810.jm@gmail.com>
2025-08-01 09:39:08 -0300

    feat: ajustar estilos de posicionamiento y ancho en el componente LogoMarquee

--- src/components/logo-marquee.tsx
+++ src/components/logo-marquee.tsx
@@ -102,13 +102,13 @@ const logos = [
 
 export default function LogoMarquee() {
     return (
-        <div className="h-auto relative mt-20 w-full xl:-left-20">
+        <div className="h-auto relative mt-20 w-full md:-left-2 2xl:-left-16">
             <div className="absolute -top-24 mx-auto flex items-center justify-center">
                 <Carousel
                     plugins={[AutoScroll({ playOnInit: true })]}
                     opts={{ loop: true, align: "start" }}
                 >
-                    <CarouselContent className="ml-0">
+                    <CarouselContent className="ml-0 w-full md:w-lg 2xl:w-3xl">
                         {logos.map((logo, index) => (
                             <CarouselItem
                                 key={index}
